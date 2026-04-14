7e52413382665dda8f61382687cbf4648a53dffa
Andrew-E-UTA <ame4230@mavs.uta.edu>
2026-04-13 23:53:08 -0500

    ROUGH implementation of the pre-processing step

--- model.py
+++ model.py
@@ -12,19 +12,22 @@
 #       image -> pre-process -> segment -> mask -> classify -> Damage Metric
 # '''
 #Basic imports
-import numpy
+import numpy as np
+import random
 import kagglehub
 import matplotlib.pyplot as plt
 
-
 #CV imports
+import cv2  
 from PIL import Image
+from skimage import exposure
 
 #system imports
 import os
 from pathlib import Path
 
 #pytorch imports
+import torch
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 
@@ -61,10 +64,8 @@ class CSE6367_Cardboardbox_dataset(Dataset):
     def __getitem__(self, idx):
         image_path = self.image_paths[idx]
         image  = Image.open(image_path).convert("RGB")
-
-        if self.transform:
-            image = self.transform(image)
-
+        image = self.transform(image)
+        image = image.numpy()
         return image    #TODO: no labels yet
 
 transform = transforms.Compose([
@@ -73,15 +74,43 @@ transform = transforms.Compose([
 ])
 
 ds1_test = CSE6367_Cardboardbox_dataset(
-    dir=data_path_1 / "cse6367_cardboardbox_damage_1" / "pakka_wala-final-dataset" / " images" / "test",
+    dir=data_path_1 / "pakka_wala-final-dataset/images/test",
     transform=transform,
 )
 
 ds2_test = CSE6367_Cardboardbox_dataset(
-    dir=data_path_2 / "Cardboard Box Defect.v8i.yolov8" / "test" / "images",
+    dir=data_path_2 / "Cardboard Box Defect.v8i.yolov8/test/images",
+    transform=transform,
+)
+ds1_train = CSE6367_Cardboardbox_dataset(
+    dir=data_path_1 / "pakka_wala-final-dataset/images/train",
     transform=transform,
 )
 
+ds2_train = CSE6367_Cardboardbox_dataset(
+    dir=data_path_2 / "Cardboard Box Defect.v8i.yolov8/train/images",
+    transform=transform,
+)
+
+def img_show(dataset):
+    n = 3
+    fig, axes = plt.subplots(1, n, figsize=(6 * n, 6))
+
+    indexes = [random.randint(0, len(dataset)) for _ in range(n)]
+
+    for ax, idx in zip(axes, indexes):
+        tensor = dataset[idx]
+        img_np = tensor.permute(1, 2, 0).numpy()
+
+        ax.imshow(img_np)
+        ax.axis("off")
+
+    plt.tight_layout()
+    plt.show()
+
+
+#show dataset is working
+# img_show(ds2_test)
 
 #==============================================================================
 #   Pre-Processing
@@ -91,6 +120,53 @@ ds2_test = CSE6367_Cardboardbox_dataset(
 # Otsu threshold
 #   - Seperate box from backgroud
 
+def preprocess(image: np.ndarray):
+    # grayscale
+    image = np.moveaxis(image, 0, -1) * 255
+    image = image.astype(np.uint8)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    #cleanup image (blur, equalize)
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))               
+    equalized = clahe.apply(blurred)    
+
+    #Morphological ops 
+    mask = cv2.adaptiveThreshold(equalized, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 15, 5)
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (9,9)) 
+    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel, iterations=2)  
+    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel, iterations=1)
+    
+    #find contour and fill
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    if contours:
+        largest = max(contours, key=cv2.contourArea)
+        solid_mask = np.zeros_like(mask)
+        cv2.drawContours(solid_mask, [largest], -1, 255, cv2.FILLED)
+        mask = solid_mask
+
+    #apply
+    masked = cv2.bitwise_and(equalized, equalized, mask=mask)
+
+    return gray, mask, masked
+
+i = ds2_test[0]#[random.randint(0,len(ds2_test))]
+e, o, m = preprocess(i)
+
+_, sub_plot = plt.subplots(1, 3)
+sub_plot[0].imshow(e, cmap='gray')
+sub_plot[0].set_title('gray_equalized')
+sub_plot[0].axis('off')
+
+sub_plot[1].imshow(o, cmap='gray')
+sub_plot[1].set_title('mask')
+sub_plot[1].axis('off')
+
+sub_plot[2].imshow(m, cmap='gray')
+sub_plot[2].set_title('masked')
+sub_plot[2].axis('off')
+
+plt.show()
 
 #==============================================================================
 #   Segmentation
